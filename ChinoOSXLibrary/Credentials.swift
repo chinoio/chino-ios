fefd9d06d6ece546352f17590578e877889e77fe
Chino <paolo@chino.io>
2017-05-16 10:35:15 +0200

    fixes

---
+++ ChinoOSXLibrary/Credentials.swift
@@ -0,0 +1,15 @@
+//
+//  Credentials.swift
+//  ChinoOSXLibrary
+//
+//  Created by Chino on 16/05/2017.
+//  Copyright © 2017 Chino. All rights reserved.
+//
+
+import Foundation
+
+open class Credentials {
+    open static let customerId = "<your-customer-id>"
+    open static let customerKey = "<your-customer-key>"
+    open static let server = "https://api.test.chino.io/v1"
+}
